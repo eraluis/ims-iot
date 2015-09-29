5ffcb9f78a32d00eada7f1974942260c93d0ee8d
eraluis <luiseraso@gmail.com>
2015-09-28 23:01:25 -0500

    Creación proyecto ims-environment-services

---
+++ ims-environment-services/src/main/java/edu/fup/ims/model/CreditCard.java
@@ -0,0 +1,12 @@
+package edu.fup.ims.model;
+
+import javax.jws.WebService;
+
+@WebService
+public class CreditCard {
+	
+	public String getCardId(String user){
+		return "1234-5678-123456";
+	}
+
+}
